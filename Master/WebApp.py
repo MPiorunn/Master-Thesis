23e0dac5fbbbc2b7571efc456573408915529ae5
Maciek <pyorunochron@gmail.com>
2019-02-13 22:01:48 +0100

    Added mongodb, able to save data inside using post method.

--- Master/WebApp.py
+++ Master/WebApp.py
@@ -1,8 +1,27 @@
-from flask import Flask
+from flask import Flask, request
+import json
+from random import randint
+import pymongo
 
 app = Flask(__name__)
+
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+
+database = myclient['master']
+collection = database['readings']
 
 
 @app.route('/')
 def hello_world():
-    return "HEJOOO"
+    file = open('stasiu/data.json').read()
+    jsoned = json.loads(file)
+    jsoned['user'] = randint(0, 10000000)
+    collection.insert(jsoned)
+    return str(jsoned)
+
+
+@app.route('/save', methods=["POST"])
+def save():
+    content = request.json
+    collection.insert(content)
+    return 'xD'
