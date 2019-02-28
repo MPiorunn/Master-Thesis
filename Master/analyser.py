0cccdd4f42fbd1374a4b4585523a8fe77453a57a
Maciej Piorun <maciej.piorun2@gmail.com>
2019-02-28 17:01:23 +0100

    Test commit

--- Master/analyser.py
+++ Master/analyser.py
@@ -23,7 +23,6 @@ def read():
 
 def forgery():
     for i in range(1, forgedFiles):
-        a = True
         name = "ja/data" + str(i) + ".txt"
         file = open(name, "r")
 
@@ -32,14 +31,20 @@ def forgery():
         sma = np.float_(file.readline().replace("SMA [", "").replace("]", "").split(", "))
         forgedTimes.append(t)
         forgedPeaks.append(signal.find_peaks(sma))
-    #     plt.subplot(files / 2, 2, i)
-    #     plt.plot(t, sma)
-    #     plt.plot(t[forgedPeaks[i - 1][0]], sma[forgedPeaks[i - 1][0]])
-    #     if i != (files - 1):
-    #         plt.xticks([], [])
+        # plt.subplot(files / 2, 2, i)
+        # plt.plot(t, sma)
+        # plt.plot(t[forgedPeaks[i - 1][0]], sma[forgedPeaks[i - 1][0]])
+        # if i != (files - 1):
+        #     plt.xticks([], [])
     # plt.show()
 
 
+
+
+
+
+minimums = []
+maximums = []
 files = 11
 forgedFiles = 6
 times = []
@@ -48,13 +53,13 @@ peaks = []
 forgedPeaks = []
 writeTimes = []
 writeFTimes = []
-print('xD')
 # read()
 distances = []
 forgedDistances = []
 avgForgedDistances = []
 read()
 forgery()
+
 for i in range(0, len(peaks)):
     tLen = len(times[i]) - 1
     writeTimes.append(times[i][tLen])
@@ -86,3 +91,17 @@ print("Median DTW : " + str(np.median(distances)))
 print(avgForgedDistances)
 writeTimes.sort()
 print(writeTimes)
+
+
+'''
+pomyśleć nad ideą rozpoznawania właściciela pisma ale zamiast podpisu odręcznego , 
+analizować po prostu jego pismo. Zebrać kilka patternów i polecieć dalej, hasłem ma być
+cokolwiek, napisane przez osobę któa chce się zaoutoryzować a my porównamy jej pismo do naszych patternó.
+poprosić o napisanie 10 wyrazów a potem złożyć z nich zdanie i postarać się rozpoznać indeksy wyrazów w zdaniu, albo i samo zdanie
+osoba robiąc enrollment wybiera sobie 10 wyrazów a jak się autoryzuje, to pisze zdanie z np. trzeba z tych wyrazów a my ich szukamy
+można też zrobić tak że im więcej szukanych wyrazów ktoś wpisze, to dostanie lepsze autoryzacje
+pattern to nie musi być linia ale zakres od-do, tutaj mogą dojść nam points of interese jeśli
+ustawimy minium na minumum, max na max a tutaj pasek jest cieniutki ;) to będzie moznaa wrzucić jako jakiś side note
+ograniczyć czas na aplikację , bo w pracy będą w tym  3-4 zdania :D
+ogólnie wygląda to w porządku, trzeba przerzucić pomysł na rozpoznawanie konkretnych wyrazów w zdaniu, podanych przez osobę przy rolloucie
+'''
