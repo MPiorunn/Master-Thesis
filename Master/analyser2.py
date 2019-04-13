d5654832abef564095c8e055e57acc569391aa42
Maciej Piorun <maciej.piorun2@gmail.com>
2019-04-13 21:11:12 +0200

    Checked rest of data. It's also wrong :(

--- Master/analyser2.py
+++ Master/analyser2.py
@@ -7,18 +7,22 @@ import json
 data = []
 
 for i in range(1, 11):
-    path = 'Raspberry/xyz/'
-    name = path + 'xyz(' + str(i) + ').json'
+    path = 'Raspberry/signatures/'
+    name = path + 'signature(' + str(i) + ').json'
     with open(name) as f:
         file = json.load(f)
         data.append(file)
 
-model = data[1]
-first = model[str(1)]
+model = data[0]
+sign = model[str(4)]
 
-plt.plot(first['t'], first['x'], 'g', label='x')
-plt.plot(first['t'], first['y'], 'r', label='y')
-plt.plot(first['t'], first['z'], 'y', label='z')
-plt.plot(first['t'], first['sma'], 'b', label='sma')
+# for i in range(0, 4):
+#     sign = model[str(i)]
+#     plt.subplot(5, 1, i + 1)
+#
+# plt.plot(first['t'], first['x'], 'g', label='x')
+# plt.plot(first['t'], first['y'], 'r', label='y')
+# plt.plot(first['t'], first['z'], 'y', label='z')
+plt.plot(sign['t'], sign['sma'], 'b', label='sma')
 plt.legend(loc='upper left')
 plt.show()
