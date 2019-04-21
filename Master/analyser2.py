83e43d7f71f35dc17e247f37f766219447dbb340
Maciej Piorun <maciej.piorun2@gmail.com>
2019-04-21 18:27:44 +0200

    Divided chart into chunks calculated area under the chars <3

--- Master/analyser2.py
+++ Master/analyser2.py
@@ -3,6 +3,7 @@ import numpy as np
 from fastdtw import fastdtw
 import scipy
 import json
+import math
 import statistics
 from pandas.io.json import json_normalize
 
@@ -55,6 +56,19 @@ def calculate_area(y, dx):
     return scipy.trapz(y, dx=dx)
 
 
+def split(a, n):
+    k, m = divmod(len(a), n)
+    return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
+
+
+def calculate_area_intervals(y, dx, intervals):
+    chunks = list(split(y, intervals))
+    areas = []
+    for chunk in chunks:
+        areas.append(calculate_area(chunk, dx))
+    return areas
+
+
 def chart_coverage(fx, up, down):
     matched = 0
     for i in range(0, len(fx)):
@@ -75,13 +89,15 @@ maxLen = 0
 # chart = data['ania'][0]
 # chart = data['piorun'][0]
 # chart = data['maciek'][0]
-# chart = data['mrozek'][0]
+chart = data['mrozek'][0]
 # chart = data['bartek'][0]
+
 # remove outliers or not?
 signatures = remove_outliers(chart)
 # signatures = chart
+
 # used to group all charts into one dataset
-results = {'pairs': []}
+# results = {'pairs': []}
 
 avgs = []
 #  sampling frequency
@@ -125,15 +141,18 @@ for i in range(1, len(averageChart)):
 
 # Calculate coverage for each chart
 averageCoverage = 0
+
 for i in signatures:
     y = signatures[str(i)]['sma']
     coverage = chart_coverage(y, upperBorder, lowerBorder)
-    print(coverage)
+    print("Coverage : " + str(coverage))
+    print("Areas : " + str(calculate_area_intervals(y, 0.025, 8)))
     averageCoverage += coverage
 
 averageCoverage /= 8
 print("Average coverage : " + str(averageCoverage))
 
+# divide charts into pieces and calculate area under the chart
 # print charts
 
 plt.plot(ts, upperBorder, marker='o', linestyle='--', color='y')
