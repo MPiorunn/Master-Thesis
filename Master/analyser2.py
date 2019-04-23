c0400c23499e03788309c1acb4094e9029dc046a
Maciej Piorun <maciej.piorun2@gmail.com>
2019-04-23 16:46:37 +0200

    Area changes in time. Polishing code

--- Master/analyser2.py
+++ Master/analyser2.py
@@ -19,8 +19,8 @@ fakes_map = {'ania': 'piorun',
 def load_data(path):
     tmp = []
     for i in range(1, 2):
-        fullPath = 'Raspberry/' + path + '/' + path
-        name = fullPath + '(' + str(i) + ').json'
+        fullPath = path + '/' + path
+        name = fullPath + '' + str(i) + '.json'
         with open(name) as f:
             tmp.append(json.load(f))
     return tmp
@@ -76,20 +76,68 @@ def chart_coverage(fx, up, down):
             matched += 1
     return matched / len(fx)
 
+def create_borders(base, interval):
+    upperBorder = [0] * len(base)
+    lowerBorder = [0] * len(base)
+    for i in range(1, len(base)):
+        base[i] /= chart_divider
+        upperBorder[i] = base[i] + interval
+        lowerBorder[i] = base[i] - interval
+        ts[i] = ts[i - 1] + freq
+    return upperBorder,lowerBorder
 
-data = {}
-for friend in friends:
-    data[friend] = load_data(friend)
-# data['xyz'] = load_data('xyz')
-# times = time_analysis(data)
-# print(times)
+def get_signatures_areas(signatures):
+    areas = []
+    for i in signatures:
+        y = signatures[str(i)]['sma']
+        area = calculate_area_intervals(y, freq, chart_divider)
+        areas.append(area)
+    return areas
+
+
+def get_signatures_coverage(signatures):
+    avg = 0
+    for i in signatures:
+        y = signatures[str(i)]['sma']
+        coverage = chart_coverage(y, upperBorder, lowerBorder)
+        avg += coverage
+    return avg / chart_divider
+
+def calculate_areas_changes(areas):
+    changes = []
+    for areaSet in areas:
+        for i in range(1, len(areaSet)):
+            print(areaSet[i]/areaSet[i-1])
+        print()
+        print()
+    return changes
+
+# globals
+# maximum data pieces in set of charts
+global maxLen
 maxLen = 0
 
+# sampling frequency
+global freq
+freq = 0.025
+
+# into how many parts do we divide each chart?
+global chart_divider
+chart_divider = 8
+
+
+data = {}
+# for friend in friends:
+# data[friend] = load_data(friend)
+# data['xyz'] = load_data('xyz')
+data['ania'] = load_data('ania')
+# times = time_analysis(data)
+# print(times)
 # which friend to check
-# chart = data['ania'][0]
+chart = data['ania'][0]
 # chart = data['piorun'][0]
 # chart = data['maciek'][0]
-chart = data['mrozek'][0]
+# chart = data['mrozek'][0]
 # chart = data['bartek'][0]
 
 # remove outliers or not?
@@ -97,7 +145,6 @@ signatures = remove_outliers(chart)
 # signatures = chart
 
 # used to group all charts into one dataset
-# results = {'pairs': []}
 
 avgs = []
 #  sampling frequency
@@ -112,8 +159,6 @@ for i in signatures:
         ts.append(t)
         t = round((t + dt), 3)
         avg[a] = y[a]
-        # results['pairs'].append({'x': t, 'y': y[a]})
-        # results['pairs'].append({'x': x[a], 'y': y[a]})
     avgs.append(avg)
     # plt.plot(x, y, marker='o', linestyle='--', color='r')
     plt.plot(ts, y, marker='o', linestyle='--', color='r')
@@ -130,49 +175,25 @@ ts = [0] * len(averageChart)
 ts[0] = 0
 # arrays with upper and lower boundaries
 interval = 0.1
-upperBorder = [0] * len(averageChart)
-lowerBorder = [0] * len(averageChart)
+
 # create average chart with boundaries
-for i in range(1, len(averageChart)):
-    averageChart[i] /= 8
-    upperBorder[i] = averageChart[i] + interval
-    lowerBorder[i] = averageChart[i] - interval
-    ts[i] = ts[i - 1] + 0.025
+(upperBorder, lowerBorder) = create_borders(averageChart,interval)
 
 # Calculate coverage for each chart
-averageCoverage = 0
-
-for i in signatures:
-    y = signatures[str(i)]['sma']
-    coverage = chart_coverage(y, upperBorder, lowerBorder)
-    print("Coverage : " + str(coverage))
-    print("Areas : " + str(calculate_area_intervals(y, 0.025, 8)))
-    averageCoverage += coverage
-
-averageCoverage /= 8
-print("Average coverage : " + str(averageCoverage))
-
 # divide charts into pieces and calculate area under the chart
+
+averageCoverage = get_signatures_coverage(signatures)
+
+areas = get_signatures_areas(signatures)
+
+changes = calculate_areas_changes(areas)
 # print charts
 
 plt.plot(ts, upperBorder, marker='o', linestyle='--', color='y')
 plt.plot(ts, averageChart, marker='o', linestyle='--', color='b')
 plt.plot(ts, lowerBorder, marker='o', linestyle='--', color='g')
 
-# sorted(results['pairs'], key=lambda i: i['x'])
-
-# xval = []
-# yval = []
-# for result in results:
-#     xval.append(result)
-#     yval.append(results[result])
-
-# xy = []
-# yy = []
-# for res in results['pairs']:
-#     xy.append(res['x'])
-#     yy.append(res['y'])
 plt.ylim(0, 2.5)
 plt.xlim(0, 2.5)
 plt.legend(loc='upper left')
-plt.show()
+# plt.show()
